b2fb1a4496ec8351721eeb0db854b395fe50389d
Raelifin <raelifin@gmail.com>
2016-02-13 12:26:43 -0500

    Can convert from 4 to IV

--- numerals_test.js
+++ numerals_test.js
@@ -2,6 +2,7 @@
 QUnit.test("Can convert from arabic to roman", function(assert) {
 	assert.equal(arabicToRoman(1), 'I');
 	assert.equal(arabicToRoman(3), 'III');
+	assert.equal(arabicToRoman(4), 'IV');
 	assert.equal(arabicToRoman(9), 'IX');
 	assert.equal(arabicToRoman(1066), 'MLXVI');
 	assert.equal(arabicToRoman(1989), 'MCMLXXXIX');
@@ -11,5 +12,6 @@ QUnit.test("Throws exception when given nonstandard input", function(assert) {
 	assert.throws(function() { arabicToRoman('hello'); });
 	assert.throws(function() { arabicToRoman(0); });
 	assert.throws(function() { arabicToRoman(-1); });
+	assert.throws(function() { arabicToRoman(1.2); });
 	assert.throws(function() { arabicToRoman(1.2); });
 });
