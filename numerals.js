aee95b2df9d9265a312625eef14563110a83ce69
Raelifin <raelifin@gmail.com>
2016-02-13 12:23:21 -0500

    Can convert from 9 to IX

--- numerals.js
+++ numerals.js
@@ -1,11 +1,34 @@
+var romanComponents = {
+	I: 1,
+	V: 5,
+	IX: 9,
+	X: 10,
+	L: 50,
+	XC: 90,
+	C: 100,
+	D: 500,
+	CM: 900,
+	M: 1000
+};
+
 function arabicToRoman(integer) {
 	if (typeof integer != 'number') { throw 'Input must be a number'; }
 	if (integer < 1) { throw 'Arabic number must be greater than 0'; }
 	if (parseInt(integer) != integer) { throw 'Arabic number must be an integer'; }
 	
 	var result = '';
-	for (var i=0; i < integer; i++) {
-		result += 'I';
+	while (integer > 0) {
+		var largest = 'I';
+		var largestVal = romanComponents[largest];
+		for (var romanSymb in romanComponents) {
+			var romanVal = romanComponents[romanSymb];
+			if (romanVal <= integer && romanVal > largestVal) {
+				largest = romanSymb;
+				largestVal = romanVal;
+			}
+		}
+		result += largest;
+		integer -= largestVal;
 	}
 	return result;
 }
--- numerals_test.js
+++ numerals_test.js
@@ -1,7 +1,8 @@
 
-QUnit.test("Can convert from 1 to I and from 3 to III", function(assert) {
+QUnit.test("Can convert from 1 to I and from 3 to III and from 9 to IX", function(assert) {
 	assert.equal(arabicToRoman(1), 'I');
 	assert.equal(arabicToRoman(3), 'III');
+	assert.equal(arabicToRoman(9), 'IX');
 });
 
 QUnit.test("Throws exception when given nonstandard input", function(assert) {
